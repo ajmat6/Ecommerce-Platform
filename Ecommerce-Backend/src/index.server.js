ed801fb1ecfbd3d24f86d23354d269a4f0eb8881
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-06-16 18:33:08 +0530

    Created json body parser for req body +  Hello API

--- Ecommerce-Backend/src/index.server.js
+++ Ecommerce-Backend/src/index.server.js
@@ -5,6 +5,23 @@ const env = require('dotenv') // importing dotenv for the port as an environment
 // Using env variable (Port no)
 env.config();
 
+// Middleware to parse the data coming as json in the req body:
+app.use(express.json());
+
+// Hello API:
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message: "Hello! This is Server"
+    })
+})
+
+// Hello API:
+app.post('/data', (req, res, next) => {
+    res.status(200).json({
+        message: req.body
+    })
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server Chal Raha He Bhai at ${process.env.PORT}`);
 })
