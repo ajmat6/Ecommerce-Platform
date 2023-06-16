caafe451b9315555748bb927c01008daf690647c
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-06-16 21:57:54 +0530

    Mongoose DB connection + mongodb database connection + user route initialization

--- Ecommerce-Backend/src/index.server.js
+++ Ecommerce-Backend/src/index.server.js
@@ -1,9 +1,25 @@
 const express = require('express');
-const app = express()
-const env = require('dotenv') // importing dotenv for the port as an environment variable
+const app = express();
+const env = require('dotenv'); // importing dotenv for the port as an environment variable
+const mongoose = require('mongoose');
+
+// Routes Import:
+const userRoutes = require('./routes/user');
 
 // Using env variable (Port no)
 env.config();
+
+// MongoDB connection
+// mongodb+srv://ajmat1130666:Ak8440800877Ak@cluster0.kvw3z7z.mongodb.net/
+mongoose.connect(
+    `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASSWORD}@cluster0.kvw3z7z.mongodb.net/`,
+    { 
+        useNewUrlParser:true,
+        useUnifiedTopology:true
+    }
+).then(() => {
+    console.log("Database Connected")
+})
 
 // Middleware to parse the data coming as json in the req body:
 app.use(express.json());
---
+++ Ecommerce-Backend/src/routes/user.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router(); // for routing
+
+// API end point for signin: POST Request
+router.get('/signin', (req,res) => {
+
+})
+
+// API end point for signup: POST Request
+router.get('/signup', (req,res) => {
+
+})
+
+module.exports = router;
