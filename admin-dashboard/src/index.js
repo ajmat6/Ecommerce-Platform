5c9713127dae81cdd0475b112d27f1b8c7422d71
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-07-13 23:22:38 +0530

    redux setup for admin dashboard

--- admin-dashboard/src/index.js
+++ admin-dashboard/src/index.js
@@ -3,15 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux'; // Provider same as useContext hook here
+import store from './store/store'; // importing store 
+
+// creating a variable for window object so that you can use it in the browser:
+window.store = store;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // from below store will be available to each component like useContext hook
+  <Provider store={store}> 
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
---
+++ admin-dashboard/src/store/store.js
@@ -0,0 +1,5 @@
+import redux from 'redux'
+
+const store = redux.legacy_createStore // to create a store
+
+export default store;
